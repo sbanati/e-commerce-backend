c7767b8837afde9025a251e16c21889a3c42a7ac
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-24 18:56:52 -0500

    connected server, added information in env and setup models for cat/product/producttag/tag and associations in index

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,31 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type:DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: id,
+        unique: false
+
+      }
+    },
+
+    tag_id : {
+      type:DataTypes.INTEGER,
+      references: {
+        model: tag,
+        key: id,
+        unique: false,
+      }
+    },
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,39 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category, foreign key used to establish the relationship
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+  
+})
+// Categories has many Products, foreign key establishes the association
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete:'CASCADE',
+})
 
-// Products belongToMany Tags (through ProductTag)
+/* Product belongToMany Tag (through ProductTag) 
+ A product can belong to multiple tag and tag can be associated with multiple products
+ through manages many-to-many associations */
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  onDelete: 'CASCADE',
+})
 
-// Tags belongToMany Products (through ProductTag)
+/*Tags belongToMany Products (through ProductTag) 
+A tag can belong to multiple product and product can be associated with multiple tag
+ProductTag model acts as a through model to manage the many-to-many relationship */
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
+
 
 module.exports = {
   Product,

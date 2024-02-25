fcaa4e0accb1c2bf35b686c46e71b4bb5cd9865a
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-24 23:39:57 -0500

    added GET request for tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,9 +3,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
+// find all tags
   // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product }]
+    });
+    res.status(200).json(tagData);
+  
+  } catch (err) {
+    console.error('Error fetching tags', err);
+    res.status(500).json({message: 'Tags could not be retrieved successfully'});
+  } 
 });
 
 router.get('/:id', (req, res) => {

7042daf6cd8b99ce3f548967f53067534d6129a5
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-24 20:31:35 -0500

    added the get routes for category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,15 +2,38 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get all Categories
+router.get('/', async (req, res) => {
+  try{
+    // find all categories including the associated Products
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json({message: 'Categories could not be retrieved successfully!'});
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Get category by id
+router.get('/:id', async (req, res) => {
+// find one category by its `id` value
+// be sure to include its associated Products
+  try{
+    // find the category by its specific id and its associated products
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [{model: Product }]
+    });
+    // if the category is not found then sesnd a 404 status with an error message
+    if (!Category){
+      res.status(404).json({message: 'error, invalid category by id search'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    // Error handling in the catch block, sending a 500 status with message
+    res.status(500).json({message: 'error, could not find!'});
+  }
 });
 
 router.post('/', (req, res) => {
